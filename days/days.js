36e92139c7fcf223585867d0da7d81cdc5414869
Tramooo <thomastramontano2000@gmail.com>
2023-09-19 20:59:27 +0200

    ok

---
+++ days/days.js
@@ -0,0 +1,14 @@
+function openmenu(){
+    document.getElementById("sidemenu").style.width = "100%";
+    document.getElementById("closemenu").style.display = "block";
+    document.getElementById("list").style.display = "block";
+    document.getElementsById("container").style.display = "none";
+    document.body.style.overflowX = "hidden";
+}
+function closemenu(){
+    document.getElementById("sidemenu").style.width = "0";
+    document.getElementById("closemenu").style.display = "none";
+    document.getElementById("list").style.display = "none";
+    document.getElementById("container").style.display = "inline-flex";
+    document.body.style.overflowX = "scroll";
+}
